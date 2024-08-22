6dc4ea1f7652a35b4f8ac699d9bfa7c70d88ab5e
mgloc <mglock@outlook.fr>
2024-08-23 01:03:01 +0700

    Added some keybindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,6 +7,12 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Exit input mode by hitting the jj
 vim.keymap.set('i', 'jj', '<Esc>')
 
+-- Using C-k and C-j for completions
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- lua/options.lua
+++ lua/options.lua
@@ -7,6 +7,14 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Tabulations
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
