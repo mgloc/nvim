8e9a0a761189078125b08872d2e52ff66fbf33f3
mgloc <mglock@outlook.fr>
2024-10-11 18:04:36 +0200

    neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -20,6 +20,13 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        never_show = {},
+      },
     },
   },
 }
