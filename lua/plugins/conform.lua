df4a9da7929e5d8db001c2991e9b684381f43647
Matteo Glock <matteo.glock@numworks.com>
2025-02-28 10:06:25 +0100

    disable autoformat binding

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,14 +18,36 @@ return {
         mode = '',
         desc = '[F]ormat buffer',
       },
+      {
+        '<leader>F',
+        function()
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+          print('Autoformat ' .. (vim.g.disable_autoformat and 'disabled' or 'enabled'))
+        end,
+        mode = '',
+        desc = '[F]ormat toggle autoformat',
+      },
     },
     opts = {
       notify_on_error = true,
-      notify_no_formatters = true,
-      format_on_save = {
-        timeout_ms = 1250,
-        lsp_fallback = 'fallback',
-      },
+      notify_no_formatters = false,
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = {}
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        -- Disable autoformat for files in a certain path
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match '/node_modules/' then
+          return
+        end
+        return { timeout_ms = 1250, lsp_format = 'fallback' }
+      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'black' },
@@ -33,7 +55,7 @@ return {
         typescript = { 'prettier' },
         ruby = { 'rubocop' },
         eruby = { 'erb_format' },
-        ylm = {},
+        yml = {},
         yaml = {},
       },
     },
