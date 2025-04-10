d3af66cb8d6a3ac57b436e78b4d794b8af1d047c
Matteo Glock <matteo.glock@numworks.com>
2025-04-10 16:59:15 +0200

    toogle csview

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -48,4 +48,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- CSVView
+vim.keymap.set('n', '<leader>tc', '<cmd>CsvViewToggle<CR>', { desc = '[T]oogle [C]SV View' })
+
 -- vim: ts=2 sts=2 sw=2 et
