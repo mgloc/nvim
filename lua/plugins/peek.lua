b2870a55f2c2c01fa9c22a8c921980c44c09942d
Matteo Glock <matteo.glock@numworks.com>
2025-06-02 14:57:12 +0200

    add bindings to toogle peek

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -7,4 +7,18 @@ return {
     vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
     vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
   end,
+  keys = {
+    {
+      '<leader>tp',
+      function()
+        if require('peek').is_open() then
+          require('peek').close()
+        else
+          require('peek').open()
+        end
+      end,
+      mode = { 'n' },
+      desc = '[T]oogle [P]eek',
+    },
+  },
 }
